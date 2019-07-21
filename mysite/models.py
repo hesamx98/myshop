24a2aaad26485219d57a9826c788804c8c306024
hesam <hesamx98@gmail.com>
2019-07-21 14:24:45 +0430

    V0

---
+++ mysite/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# class payment(models.Model):
+#     insurance = models.CharField(max_length=200)
+#     phone = models.CharField(max_length=200)
+#     date = models.CharField(max_length=200)
+#     price = models.CharField(max_length=200)
+#     status = models.CharField(max_length=200)
+#     pay_id = models.CharField(max_length=200)
+#     description = models.CharField(max_length=200)
