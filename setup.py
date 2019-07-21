e01940fc990e9c43670b9e69aeb9f5b1716c04a9
hesam <hesamx98@gmail.com>
2019-07-21 15:20:50 +0430

    V0

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='myshop',
+    version='0',
+    packages=['myshop', 'mysite', 'mysite.migrations'],
+    url='',
+    license='',
+    author='hesam',
+    author_email='hesamx98@gmail.com',
+    description=''
+)
